f09e4b46c4505a1e51044ff79eb60a39d369c138
Alejo <70706948+VictiniFan360@users.noreply.github.com>
2026-01-06 21:34:23 +0100

    Actualizar

--- footer.php
+++ footer.php
@@ -22,41 +22,7 @@
                 ?>
             </section>
 
-            <!-- Columna 2: Soporte -->
-            <section class="footer-column">
-                <h3 class="footer-title"><?php _e('Soporte', 'semantic-wp'); ?></h3>
-                <?php
-                if ( has_nav_menu( 'footer_support' ) ) {
-                    wp_nav_menu([
-                        'theme_location' => 'footer_support',
-                        'container'      => false,
-                        'menu_class'     => 'footer-menu',
-                        'depth'          => 1,
-                    ]);
-                } else {
-                    semantic_wp_footer_fallback( 'footer_support' );
-                }
-                ?>
-            </section>
-
-            <!-- Columna 3: Colaborá -->
-            <section class="footer-column">
-                <h3 class="footer-title"><?php _e('Colaborá', 'semantic-wp'); ?></h3>
-                <?php
-                if ( has_nav_menu( 'footer_collab' ) ) {
-                    wp_nav_menu([
-                        'theme_location' => 'footer_collab',
-                        'container'      => false,
-                        'menu_class'     => 'footer-menu',
-                        'depth'          => 1,
-                    ]);
-                } else {
-                    semantic_wp_footer_fallback( 'footer_collab' );
-                }
-                ?>
-            </section>
-
-            <!-- Columna 4: Acerca de -->
+            <!-- Columna 2: Acerca de -->
             <section class="footer-column">
                 <h3 class="footer-title"><?php _e('Acerca de', 'semantic-wp'); ?></h3>
                 <?php
